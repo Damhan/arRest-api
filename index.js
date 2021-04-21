604b8beafe2c7790e7ccc7e7f633226ab54cc4e4
Damhan Richardson <damhanrichardson@Gmail.com>
2021-04-20 19:08:06 -0700

    Added GET for arrest by ID

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ app.get('/listArrests', (req,res) => {
     })
 })
 
+app.get('/:key', (req,res) => {
+    fs.readFile(__dirname + "/" + "nypd-arrest-data-sample.json", "utf-8", (err,data) => {
+        if(err) 
+            console.log(err);
+        else {
+            var arrests = JSON.parse(data);
+            var arrest = arrests.find(arr => {return arr.ARREST_KEY == req.params.key})
+            res.end(JSON.stringify(arrest));
+        }
+    })
+})
+
 var server = app.listen(8081, () => {
     var host = server.address().address
     var port = server.address().port
